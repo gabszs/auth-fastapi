ae1755e0a4453484a1a9225bd7fb0dccef0b28c3
GabrielDeCarvalho <gnorberc@emeal.nttdata.com>
2025-07-01 14:47:41 -0300

    feat(traced_id): caching the trace_id

--- app/routes/v1/background_routes.py
+++ app/routes/v1/background_routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from fastapi import BackgroundTasks
+from opentelemetry import trace
 
 from app.core.telemetry import instrument
 from app.core.telemetry import logger
@@ -10,6 +11,10 @@ router = APIRouter(prefix="/background", tags=["Background"])
 
 @instrument
 def write_log(email: str):
+    span = trace.get_current_span()
+    trace_id = format(span.get_span_context().trace_id, "032x")
+
+    logger.info(f"TraceId caught: {trace_id}")
     logger.info(f"Email task succesifully received, email: {email}", extra={"email": email})
 
 
