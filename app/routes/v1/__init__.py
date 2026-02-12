60d5def496c309c8a0e4ba304a181f7e35842434
GabrielDeCarvalho <gnorberc@emeal.nttdata.com>
2026-02-12 17:32:34 -0300

    test

--- app/routes/v1/__init__.py
+++ app/routes/v1/__init__.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
 from app.routes.v1.auth_routes import router as auth_router
+from app.routes.v1.password_routes import router as password_router
 from app.routes.v1.users_routes import router as user_router
 
 routers = APIRouter(prefix="/v1")
-router_list = [auth_router, user_router]
+router_list = [auth_router, user_router, password_router]
 
 for router in router_list:
     routers.include_router(router)
